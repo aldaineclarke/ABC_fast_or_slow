3804e1a435fb505fbd8dcecdd740d5166d79377b
Rajeime <rajeime.m.roberts@gmail.com>
2023-07-28 14:14:56 -0500

    chore: update app module to connect to server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GameModule } from './modules/game/game.module';
+import { SocketIoModule } from 'ngx-socket-io';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    GameModule
+    GameModule,
+    SocketIoModule.forRoot({
+      url: 'http://localhost:6060', //server url -- should be updated to match server
+      options: {
+        transports: ['websocket'],
+      },
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
