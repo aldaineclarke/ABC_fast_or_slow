b83606fc077208df927f4e1030a8c914d92ba8d9
Aldaine-dell <aldaineclarke@myambergroup.com>
2023-07-29 17:26:30 -0500

    created main page component

--- src/app/modules/game/game.module.ts
+++ src/app/modules/game/game.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    MainPageComponent
   ],
   imports: [
     CommonModule
