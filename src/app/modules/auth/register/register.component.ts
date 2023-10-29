54fd021973f98f8a68ac377d76edf941b40d7c0e
Aldaine-Legion <vainedev@gmail.com>
2023-10-29 16:44:12 -0500

    updated register component to accept first and lastname as required parameters

--- src/app/modules/auth/register/register.component.ts
+++ src/app/modules/auth/register/register.component.ts
@@ -15,6 +15,8 @@ export class RegisterComponent {
   notif = inject(ToastrService)
   apiUrl = environment.apiUrl;
   registrationForm = new FormGroup({
+    "firstName": new FormControl("", [Validators.required]),
+    "lastName": new FormControl("", [Validators.required]),
     "username": new FormControl("", [Validators.required]),
     "email": new FormControl("", [Validators.required]),
     "password": new FormControl("", [Validators.required]),
