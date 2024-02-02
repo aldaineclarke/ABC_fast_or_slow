8555343dc46841541996ed31a938780f479a8ed8
Aldaine-Legion <vainedev@gmail.com>
2024-02-02 17:46:41 -0500

    killed the loader after error occurs with error interceptor

--- src/app/interceptors/error.interceptor.ts
+++ src/app/interceptors/error.interceptor.ts
@@ -8,15 +8,19 @@ import {
 } from '@angular/common/http';
 import { Observable, catchError, throwError } from 'rxjs';
 import { ToastrService } from 'ngx-toastr';
+import { LoadingService } from '../services/loading.service';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
   toastr = inject(ToastrService);
+  loaderService = inject(LoadingService);
   constructor() {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     return next.handle(request).pipe(
       catchError((response:HttpErrorResponse)=>{
+        this.loaderService.killLoader();
+
         let errorMessage = `Error: ${response.error.message}`;
         if(response.error.errors.length > 0 && typeof response.error.errors[0] == "string"){
           errorMessage = response.error.errors[0];
